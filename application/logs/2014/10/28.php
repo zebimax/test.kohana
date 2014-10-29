404e5e9c1c3296fcdaed6a26a4a93afb53538916
zebimax <zebimax@gmail.com>
2014-10-29 12:41:26 +0200

    Add and setting kohana.

---
+++ application/logs/2014/10/28.php
@@ -0,0 +1,5 @@
+<?php defined('SYSPATH') OR die('No direct script access.'); ?>
+
+2014-10-28 23:23:20 --- CRITICAL: ErrorException [ 4 ]: syntax error, unexpected ',' ~ xdebug://debug-eval [ 1 ] in file:line
+2014-10-28 23:23:20 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in file:line
